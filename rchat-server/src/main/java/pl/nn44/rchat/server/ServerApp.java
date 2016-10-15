8b54debefd4e157cfb23ceecfc73f34eedba2bce
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-10-15 18:26:55 +0200

    server: xml-rpc bean is now created in the same way as hessian&burlap

--- rchat-server/src/main/java/pl/nn44/rchat/server/ServerApp.java
+++ rchat-server/src/main/java/pl/nn44/rchat/server/ServerApp.java
@@ -1,6 +1,11 @@
 package pl.nn44.rchat.server;
 
 import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.metadata.XmlRpcSystemImpl;
+import org.apache.xmlrpc.server.PropertyHandlerMapping;
+import org.apache.xmlrpc.server.XmlRpcErrorLogger;
+import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
+import org.apache.xmlrpc.webserver.XmlRpcServletServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.ErrorController;
@@ -8,11 +13,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.remoting.caucho.BurlapServiceExporter;
 import org.springframework.remoting.caucho.HessianServiceExporter;
+import org.springframework.web.HttpRequestHandler;
 import pl.nn44.rchat.protocol.ChatService;
 import pl.nn44.rchat.server.impl.BestChatService;
 import pl.nn44.rchat.server.page.MainPageController;
 import pl.nn44.rchat.server.page.PlainErrorController;
-import pl.nn44.rchat.server.xmlrpc.XmlRpcController;
 
 @Configuration
 @EnableAutoConfiguration
@@ -28,7 +33,7 @@ public class ServerApp {
     }
 
     @Bean(name = "/hessian")
-    public HessianServiceExporter hessianService() {
+    public HttpRequestHandler hessianChatController() {
         HessianServiceExporter exporter = new HessianServiceExporter();
         exporter.setService(chatService());
         exporter.setServiceInterface(ChatService.class);
@@ -36,23 +41,35 @@ public class ServerApp {
     }
 
     @Bean(name = "/burlap")
-    public BurlapServiceExporter burlapService() {
+    public HttpRequestHandler burlapChatController() {
         BurlapServiceExporter exporter = new BurlapServiceExporter();
         exporter.setService(chatService());
         exporter.setServiceInterface(ChatService.class);
         return exporter;
     }
 
-    @Bean
-    public XmlRpcController xmlRpcService() throws XmlRpcException {
+    @Bean(name = "/xml-rpc")
+    public HttpRequestHandler xmlRpcChatController() throws XmlRpcException {
         int cores = Runtime.getRuntime().availableProcessors();
 
-        return new XmlRpcController(
-                "ChatService",
-                ChatService.class,
-                chatService(),
-                cores * 5
-        );
+        XmlRpcServerConfigImpl config = new XmlRpcServerConfigImpl();
+        config.setEncoding(XmlRpcServerConfigImpl.UTF8_ENCODING);
+        config.setEnabledForExceptions(true);
+        config.setEnabledForExtensions(true);
+        config.setKeepAliveEnabled(true);
+
+        PropertyHandlerMapping handlerMapping = new PropertyHandlerMapping();
+        handlerMapping.setRequestProcessorFactoryFactory(pClass -> pRequest -> chatService());
+        handlerMapping.addHandler("ChatService", ChatService.class);
+        XmlRpcSystemImpl.addSystemHandler(handlerMapping);
+
+        XmlRpcServletServer server = new XmlRpcServletServer();
+        server.setConfig(config);
+        server.setErrorLogger(new XmlRpcErrorLogger());
+        server.setMaxThreads(cores * 5);
+        server.setHandlerMapping(handlerMapping);
+
+        return server::execute;
     }
 
     @Bean
