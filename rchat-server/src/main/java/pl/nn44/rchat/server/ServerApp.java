bc45751d8ae34de979084c43e39ee44da5be0b12
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-11-09 20:13:39 +0100

    server: add info about user agent to log output

--- rchat-server/src/main/java/pl/nn44/rchat/server/ServerApp.java
+++ rchat-server/src/main/java/pl/nn44/rchat/server/ServerApp.java
@@ -1,5 +1,6 @@
 package pl.nn44.rchat.server;
 
+import ch.qos.logback.classic.helpers.MDCInsertingServletFilter;
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.metadata.XmlRpcSystemImpl;
 import org.apache.xmlrpc.server.PropertyHandlerMapping;
@@ -7,12 +8,12 @@ import org.apache.xmlrpc.server.XmlRpcErrorLogger;
 import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
 import org.apache.xmlrpc.webserver.XmlRpcServletServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.remoting.caucho.BurlapServiceExporter;
 import org.springframework.remoting.caucho.HessianServiceExporter;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.HttpRequestHandler;
 import pl.nn44.rchat.protocol.ChatService;
 import pl.nn44.rchat.server.impl.BestChatService;
@@ -20,8 +21,10 @@ import pl.nn44.rchat.server.page.MainPageController;
 import pl.nn44.rchat.server.page.PlainErrorController;
 import pl.nn44.xmlrpc.AnyTypeFactory;
 
-@Configuration
-@EnableAutoConfiguration
+import javax.servlet.Filter;
+
+@SpringBootApplication
+@EnableScheduling
 public class ServerApp {
 
     public static void main(String[] args) {
@@ -83,5 +86,10 @@ public class ServerApp {
     @Bean
     public ErrorController errorPageController() {
         return new PlainErrorController();
+    }
+
+    @Bean
+    public Filter mdcInsertingServletFilter() {
+        return new MDCInsertingServletFilter();
     }
 }
